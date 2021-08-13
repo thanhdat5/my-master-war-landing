f87054f7ebbbf6e3fc935d529525fd31a7d710d5
OHT <nguyenthanhdat.dcna@gmail.com>
2021-08-13 14:37:26 +0700

    Update teams

--- components/Loading/index.tsx
+++ components/Loading/index.tsx
@@ -9,7 +9,7 @@ export default function LoadingComponent() {
                 $('#MMW_loading').fadeOut(400);
             }, 500);
         }
-    }, [])
+    })
 
     return <div className={styles.tq_loading} id="MMW_loading">
         <img src="/images/loading.gif" alt="loading..." height="90" />
